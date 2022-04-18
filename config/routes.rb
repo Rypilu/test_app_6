a4e013ed39ffd14da28eb2d6504de37804d89694
rypilu <rpiluski@gmail.com>
2022-04-18 06:33:48 -0400

    CRUD and scaffold generators

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   # this is app/controllers/application_controller.rb -- rails understands application to be application_controller.rb
   root 'pages#home'
   get 'about', to: 'pages#about'
